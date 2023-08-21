4e6069f77e37a5fcb09424551f90a87bd119d623
ujw0704 <ujwalsharma571@gmail.com>
2023-08-21 23:14:21 +0530

    first commit

---
+++ src/Components/Form.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+ import axios from "axios"
+function Form() {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+ function handelsubmit(e){
+    e .preventDefault()
+ console.log("name",name)
+ console.log("password",password)
+
+ }   
+
+ axios.post('http://localhost:8080/login',{name, password})
+ .then((resp)=>{
+    console.log(resp)
+
+ })
+
+ 
+    return (
+        <div>
+            <form onSubmit={handelsubmit}>
+
+           <input className='name' type="text" placeholder='Enter Your name' value={name} onChange={e => setName(e.target.value)} name="name" />
+           <input className='name' type="text" placeholder='Enter Your password' value={password} onChange={e => setPassword(e.target.value)} password="password" />
+           <button type='submit'>submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
