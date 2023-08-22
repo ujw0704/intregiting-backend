f0cf95a8dc706e568fe577c1d721df5489abeb44
ujw0704 <ujwalsharma571@gmail.com>
2023-08-22 18:50:03 +0530

    finished

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
- import axios from "axios"
+import axios from "axios"
+
 function Form() {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
 
- function handelsubmit(e){
-    e .preventDefault()
- console.log("name",name)
- console.log("password",password)
 
- }   
+    function handelsubmit(e) {
+        e.preventDefault();
+        console.log(name, password)
 
- axios.post('http://localhost:8080/login',{name, password})
- .then((resp)=>{
-    console.log(resp)
+        
+        axios.post('http://localhost:8080/login', {name, password})
+            .then((result) => {
+                console.log(result.data)
+                setName("")
+                setPassword("")
 
- })
+            })
+    }
 
- 
+console.log(name)
     return (
         <div>
             <form onSubmit={handelsubmit}>
 
-           <input className='name' type="text" placeholder='Enter Your name' value={name} onChange={e => setName(e.target.value)} name="name" />
-           <input className='name' type="text" placeholder='Enter Your password' value={password} onChange={e => setPassword(e.target.value)} password="password" />
-           <button type='submit'>submit</button>
+                <input className='name' type="text" placeholder='Enter Your name' value={name} onChange={e => setName(e.target.value)} name="name" />
+                <input className='name' type="text" placeholder='Enter Your password' value={password} onChange={e => setPassword(e.target.value)} password="password" />
+                <button type='submit'>submit</button>
             </form>
         </div>
     );
